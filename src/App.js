9a450e2d9bb620f84ce6da9e3ce8086d57df7b87
Evoxus <gamersnowman@gmail.com>
2018-12-12 17:24:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+// let quotes = (function() {
+//   let quotes = null;
+//   $.ajax({
+//     async: false,
+//     global: false,
+//     url: "quotes.json",
+//     dataType: "json",
+//     success: function(data) {
+//       quotes = data;
+//     }
+//   });
+//   return quotes;
+// })();
+
+// console.log(quotes);
+
+class QuoteBox extends Component {
+  render() {
+    return <p> Testing </p>;
+  }
+}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <QuoteBox />;
   }
 }
 
