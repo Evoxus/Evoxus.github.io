8f2d7e98b8edd14a353ee9fa59e8074e51ebe0b8
Evoxus <gamersnowman@gmail.com>
2018-12-13 12:32:56 -0700

    removed unnecessary packages, structured state for quotes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-import React, {
-  Component
-} from "react";
+import React, { Component } from "react";
 import "./App.css";
 import quotes from './quotes';
 
 class QuoteBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: quotes
+    }
+    this.randomNumber = this.randomNumber.bind(this);
+  }
+
+  // generates a random number between 0 and 102 (there are 102 different quotes to pick from)
+  randomNumber() {
+    return Math.round(Math.random() * 102)
+  }
+
   render() {
-    const newQuote = randomNumber();
-    console.log(newQuote);
-    return <div>
-      <p>{quotes[newQuote].quote}
-        <caption>{quotes[newQuote].author}</caption>
-      </p>
-    </div>
+    const newQuote = this.randomNumber();
+    return (
+      <div id='quote-box'>
+        <h2 id='text'>{quotes[newQuote].quote}</h2>
+        <p id='author'>{quotes[newQuote].author}</p>
+      </div>
+    );
   }
 }
 
 class App extends Component {
   render() {
-    return <QuoteBox />;
+    return (
+      <QuoteBox />
+    );
   }
-}
-
-function randomNumber() {
-  return Math.round(Math.random() * 102)
 }
 
 export default App;
