2d423a75fcba600098acbb43050e52deb43485bf
Evoxus <gamersnowman@gmail.com>
2018-12-18 18:31:13 -0700

    Beggining to style, finished main functionality

--- src/App.js
+++ src/App.js
@@ -6,25 +6,37 @@ class QuoteBox extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: quotes
+      quotes: quotes,
+      currentQuote: Math.round(Math.random() * 102)
     }
     this.randomNumber = this.randomNumber.bind(this);
   }
 
   // generates a random number between 0 and 102 (there are 102 different quotes to pick from)
   randomNumber() {
-    return Math.round(Math.random() * 102)
+    this.setState({
+      currentQuote: Math.round(Math.random() * 102)
+    })
   }
 
   render() {
-    const newQuote = this.randomNumber();
+
     return (
-      <div id='quote-box'>
-        <h2 id='text'>{quotes[newQuote].quote}</h2>
-        <p id='author'>{quotes[newQuote].author}</p>
+      <div id='quote-box' className="container">
+        <QuoteText value={this.state} />
+        <QuoteAuthor value={this.state} />
+        <button id="new-quote" className="btn btn-primary" onClick={this.randomNumber}>New Quote</button>
       </div>
     );
   }
+}
+
+function QuoteText(props) {
+  return <h2 id="text">{props.value.quotes[props.value.currentQuote].quote}</h2>;
+}
+
+function QuoteAuthor(props) {
+  return <p id='author'>{props.value.quotes[props.value.currentQuote].author}</p>;
 }
 
 class App extends Component {
