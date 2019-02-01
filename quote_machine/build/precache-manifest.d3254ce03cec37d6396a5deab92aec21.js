facc75d7d0d68687ee178b7cd958201f1deca3f8
Evoxus <gamersnowman@gmail.com>
2019-02-01 11:59:10 -0700

    built quoteMachine

---
+++ quote_machine/build/precache-manifest.d3254ce03cec37d6396a5deab92aec21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "869912630b29eb7db91c",
+    "url": "/static/js/main.86991263.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "869912630b29eb7db91c",
+    "url": "/static/css/main.508bc28a.chunk.css"
+  },
+  {
+    "revision": "802dc00394e4cbbc477a2b94ed15dc47",
+    "url": "/index.html"
+  }
+];
