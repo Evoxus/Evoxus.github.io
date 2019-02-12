4816de65c9c15d578cb311c54b2cb390896a238c
Evoxus <gamersnowman@gmail.com>
2019-02-11 20:15:01 -0700

    Static page working

---
+++ quote_machine/build/1-fb01b91297ff801fd37f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{133:function(p){p.exports={pages:[],dataPaths:{}}}}]);
+//# sourceMappingURL=1-fb01b91297ff801fd37f.js.map
---
+++ quote_machine/build/precache-manifest.cca53c00714d65a0cc16fbf54172cbd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bacf303fb3efbb630ed7",
+    "url": "/quote_machine/static/js/runtime~main.bacf303f.js"
+  },
+  {
+    "revision": "458073b9ba9d9401e500",
+    "url": "/quote_machine/static/js/main.458073b9.chunk.js"
+  },
+  {
+    "revision": "cd2e4a6d898a83ec9868",
+    "url": "/quote_machine/static/js/2.cd2e4a6d.chunk.js"
+  },
+  {
+    "revision": "458073b9ba9d9401e500",
+    "url": "/quote_machine/static/css/main.84ebd671.chunk.css"
+  },
+  {
+    "revision": "5c9a746d5f5dc7ab09ecfca29b1e5869",
+    "url": "/quote_machine/index.html"
+  }
+];
